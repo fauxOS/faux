d02b2f77aa8863da6f5a28506f04a2bac004997f
Daniel E <danielethridge@icloud.com>
2017-03-10 14:30:42 -0500

    Update filedesc.js

--- src/kernel/proc/filedesc.js
+++ src/kernel/proc/filedesc.js
@@ -1,6 +1,32 @@
-class FileDesc {
+// "Everything is a file"
+// This is the layer that makes that idea possible
+
+class FileDescriptor {
   constructor(path) {
-    this.path = path;
-    this.inode = faux.fs.resolve(this.path);
+    this.path = new Pathname(path).clean;
+    this.type = faux.fs.type(this.path);
+    this.container = faux.fs.resolve(this.path);
+    if (this.container < 0) {
+      throw new Error("Path Unresolved");
+    }
+  }
+
+  // Return read data
+  read() {
+    if (this.type === "inode") {
+      const data = this.container.data;
+      // Directory or other
+      if (data == null) {
+        return -1;
+      }
+      return data;
+    }
+  }
+
+  // Write data out
+  write(data) {
+    if (this.type === "inode") {
+      return this.container.data = data;
+    }
   }
 }
