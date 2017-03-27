8fa3bfa19cec6b52084972d8082df9ee8d742922
Daniel E <danielethridge@icloud.com>
2017-03-27 10:56:45 -0400

    add getenv() and setenv()

--- src/kernel/proc/syscalls.js
+++ src/kernel/proc/syscalls.js
@@ -39,21 +39,19 @@ sys.open = function(process, msgID, args) {
   if (args.length !== 1) {
     sys.fail(process, msgID, ["Should have only 1 argument"]);
   }
-  else if (typeof args[0] !== "string") {
+  if (typeof args[0] !== "string") {
     sys.fail(process, msgID, ["Argument should be a string"]);
   }
-  else {
-    let path = "";
-    // If the first character is a "/", then working dir does not matter
-    if (args[0][0] === "/") {
-      path = args[0];
-    }
-    else {
-      path = process.cwd + "/" + args[0];
-    }
-    const result = process.open(path);
-    sys.pass(process, msgID, [result]);
+  let path = "";
+  // If the first character is a "/", then working dir does not matter
+  if (args[0][0] === "/") {
+    path = args[0];
   }
+  else {
+    path = process.cwd + "/" + args[0];
+  }
+  const result = process.open(path);
+  sys.pass(process, msgID, [result]);
 }
 
 // Read data from a file descriptor
@@ -61,13 +59,11 @@ sys.read = function(process, msgID, args) {
   if (args.length !== 1) {
     sys.fail(process, msgID, ["Should have only 1 argument"]);
   }
-  else if (args[0] < 0) {
+  if (args[0] < 0) {
     sys.fail(process, msgID, ["File Descriptor should be postive"]);
   }
-  else {
-    const result = process.fds[ args[0] ].read();
-    sys.pass(process, msgID, [result]);
-  }
+  const result = process.fds[ args[0] ].read();
+  sys.pass(process, msgID, [result]);
 }
 
 // Write data to a file descriptor
@@ -75,13 +71,11 @@ sys.write = function(process, msgID, args) {
   if (args.length !== 2) {
     sys.fail(process, msgID, ["Should have 2 arguments"]);
   }
-  else if (args[0] < 0) {
+  if (args[0] < 0) {
     sys.fail(process, msgID, ["File Descriptor should be postive"]);
   }
-  else {
-    const result = process.fds[ args[0] ].write( args[1] );
-    sys.pass(process, msgID, [result]);
-  }
+  const result = process.fds[ args[0] ].write( args[1] );
+  sys.pass(process, msgID, [result]);
 }
 
 // Change the current working directory
@@ -89,10 +83,29 @@ sys.chdir = function(process, msgID, args) {
   if (! args[0] instanceof String) {
     sys.fail(process, msgID, ["Argument should be a string"]);
   }
-  else {
-    process.cwd = args[0];
-    sys.pass(process, msgID, [ process.cwd ]);
+  process.cwd = args[0];
+  sys.pass(process, msgID, [ process.cwd ]);
+}
+
+// Get environment variable
+sys.getenv = function(process, msgID, args) {
+  if (! args[0] instanceof String) {
+    sys.fail(process, msgID, ["Variable name should be a string"]);
   }
+  const value = process.env[ args[0] ];
+  sys.pass(process, msgID, [ value ]);
+}
+
+// Set environment variable
+sys.setenv = function(process, msgID, args) {
+  if (! args[0] instanceof String) {
+    sys.fail(process, msgID, ["Variable name should be a string"]);
+  }
+  if (! args[1] instanceof String) {
+    sys.fail(process, msgID, ["Variable value should be a string"]);
+  }
+  const value = process.env[ args[0] ] = args[1];
+  sys.pass(process, msgID, [ value ]);
 }
 
 export default sys;
