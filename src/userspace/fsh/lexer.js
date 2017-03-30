391fad6c2f1f355d72f544c1b888bc2e484acc84
Daniel E <danielethridge@icloud.com>
2017-03-30 14:30:48 -0400

    don't look

--- src/userspace/fsh/lexer.js
+++ src/userspace/fsh/lexer.js
@@ -1,6 +1,39 @@
 // Eventually, this should be a complete
-// lexer, but for now, it just does a whitespace split...
+// lexer, but for now, it basically just does a whitespace split...
 
-export default function lex(input="") {
-  return input.split( /[\s]+/ig );
+function split(input="") {
+  return input
+    .split( /(?:[^\\])(;)/g );
 }
+
+function lex(line="") {
+  const tokens = line
+    .match( /(["'])([^\1\\])*?\1|((?:[^\\\s;]|\\.)*)/g )
+    .filter(String);
+  for (let i=0; i < tokens.length; i++) {
+    let token = tokens[i];
+    tokens[i] = token.replace(/\\(?=.)/g, "");
+    if ( token.match(/^["'].+(\1)$/m) ) {
+      tokens[i] = ( /^(["'])(.+)(\1)$/gm ).exec(token)[2];
+    }
+  }
+  return tokens;
+}
+
+function parse(input="") {
+  const allTokens = [];
+  const lines = split(input);
+  for (let i in lines) {
+    let tokens = lex( lines[i] );
+    allTokens.push( tokens );
+  }
+  return allTokens;
+}
+
+/* Test against this block
+
+some\ spaces\ here should not "matter 'at all'";
+./command one; two | piped | over > here;
+false || true && echo "anything\"\ ";
+
+*/
