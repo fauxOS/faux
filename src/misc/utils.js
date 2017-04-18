55a0b7e2192bb16c617a78058df3a705d15ec90f
Daniel Ethridge <danielethridge@icloud.com>
2017-04-18 11:09:56 -0400

    added yarn and built

--- src/misc/utils.js
+++ src/misc/utils.js
@@ -1,13 +1,12 @@
 const utils = {};
 
 utils.genUUID = function() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-    /[xy]/g,
-    function(char) {
-      let r = Math.random() * 16 | 0, v = char === "x" ? r : r & 0x3 | 0x8;
-      return v.toString(16);
-    }
-  );
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(
+    char
+  ) {
+    let r = (Math.random() * 16) | 0, v = char === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 };
 
 utils.mkWorker = function(scriptStr) {
