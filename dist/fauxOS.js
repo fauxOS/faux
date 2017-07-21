f9baffba2a6e50a61bd08dab97ecccf4194374fe
Daniel Ethridge <danielethridge@icloud.com>
2017-07-21 00:21:13 -0400

    Console reads bytes, not events

--- dist/fauxOS.js
+++ dist/fauxOS.js
@@ -452,17 +452,16 @@
             return ret;
         }
     }
-    class Termios {
+    class Console {
         constructor(config = {}) {
             // This line buffer is used so that the user can edit
             // typing mistakes before the input is read by a program
             this.lineBuffer = [];
             // The string that will be returned on this.read()
             this.input = "";
-            // Here is the termios configuration
             this.config = {
-                // Whether termios should be active at all.
-                // If this is set false, termios won't buffer and allow line editing
+                // Whether this should be active at all.
+                // If buffer is set false, line editing will be skipped
                 buffer: true,
                 // When a user types, should they see their input?
                 // Setting this false is useful for e.g. password input
@@ -476,43 +475,46 @@
             this.input = "";
             return ret;
         }
-        // Takes a KeyboardEvent and decides what to do
-        send(e) {
-            const { which } = e;
-            const char = String.fromCharCode(which);
+        // Clients should override this
+        write(contents) {
+            console.log("Unhandled console write: " + contents);
+        }
+        // Takes the key pressed and decides what to do
+        send(key, e) {
             // Handle input normally
             if (this.config.buffer) {
-                this.handle(char);
+                this.handle(key);
             }
             else {
                 // Without buffering, this is just a simple relay
-                this.input += char;
+                this.input += key;
             }
             // Echo input to the terminal so the user sees
             // what is being typed
             if (this.config.echo) {
-                console$1.write(char);
+                this.write(key);
             }
         }
         // If the character is special, handle it.
         // If it is normal, just push it to the lineBuffer
-        handle(char) {
-            if (char === "\b") {
+        handle(key) {
+            // Handle the DELETE sequence `^?` rather than backspace
+            if (key === "\x7f") {
                 this.backSpace();
             }
-            else if (char === "\r") {
+            else if (key === "\r") {
                 this.enter();
             }
             else {
                 // Normal character, just push it to the lineBuffer
-                this.lineBuffer.push(char);
+                this.lineBuffer.push(key);
             }
         }
         // Discard last written character
         backSpace() {
             this.lineBuffer.pop();
             // Back one, overwrite with space, then back once more
-            console$1.write("\b \b");
+            this.write("\b \b");
         }
         // Save the last line and start a new one
         enter(shiftKey) {
@@ -523,20 +525,7 @@
             // Clear the lineBuffer
             this.lineBuffer = [];
             // Carriage return and line feed
-            console$1.write("\r\n");
-        }
-    }
-    class Console {
-        constructor() {
-            this.termios = new Termios();
-        }
-        // Relay reads through termios
-        read() {
-            return this.termios.read();
-        }
-        // Clients should override this
-        write(contents) {
-            console.log("Unhandled console write: " + contents);
+            this.write("\r\n");
         }
     }
     var console$1 = new Console();
--- src/kernel/fs/dev/console/index.js
+++ src/kernel/fs/dev/console/index.js
@@ -1,18 +1,82 @@
-import Termios from "./termios.js";
-
 class Console {
-  constructor() {
-    this.termios = new Termios();
+  constructor(config = {}) {
+    // This line buffer is used so that the user can edit
+    // typing mistakes before the input is read by a program
+    this.lineBuffer = [];
+    // The string that will be returned on this.read()
+    this.input = "";
+    this.config = {
+      // Whether this should be active at all.
+      // If buffer is set false, line editing will be skipped
+      buffer: true,
+      // When a user types, should they see their input?
+      // Setting this false is useful for e.g. password input
+      echo: true
+    };
+    Object.assign(this.config, config);
   }
 
-  // Relay reads through termios
+  // Clear and return this.input
   read() {
-    return this.termios.read();
+    const ret = this.input;
+    this.input = "";
+    return ret;
   }
 
   // Clients should override this
   write(contents) {
     console.log("Unhandled console write: " + contents);
+  }
+
+  // Takes the key pressed and decides what to do
+  send(key, e) {
+    // Handle input normally
+    if (this.config.buffer) {
+      this.handle(key);
+    } else {
+      // Without buffering, this is just a simple relay
+      this.input += key;
+    }
+    // Echo input to the terminal so the user sees
+    // what is being typed
+    if (this.config.echo) {
+      this.write(key);
+    }
+  }
+
+  // If the character is special, handle it.
+  // If it is normal, just push it to the lineBuffer
+  handle(key) {
+    // Handle the DELETE sequence `^?` rather than backspace
+    if (key === "\x7f") {
+      this.backSpace();
+    } else if (key === "\r") {
+      this.enter();
+    } else {
+      // Normal character, just push it to the lineBuffer
+      this.lineBuffer.push(key);
+    }
+  }
+
+  // Discard last written character
+  backSpace() {
+    this.lineBuffer.pop();
+    // Back one, overwrite with space, then back once more
+    this.write("\b \b");
+  }
+
+  // Save the last line and start a new one
+  enter(shiftKey) {
+    this.lineBuffer.push("\n");
+
+    // Push the lineBuffer away
+    const line = this.lineBuffer.join("");
+    this.input += line;
+    // Clear the lineBuffer
+    this.lineBuffer = [];
+
+    // Carriage return and line feed
+    this.write("\r\n");
   }
 }
 
